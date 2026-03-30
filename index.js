aab2d2fd6eaa085bfe4fec45641162241c37687c
Ev-23WDA <evelacioabreu@gmail.com>
2026-03-30 12:05:10 +0100

    JS ramdom panels

--- index.js
+++ index.js
@@ -3,10 +3,40 @@ const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const blue = document.querySelector('.blue');
 
+const getRandomPanel = () => {
+    const panels = [
+    green,
+    red,
+    yellow,
+    blue
+];
+return panels[parseInt(Math.random() * panels.length)];
+};
+
+
 const sequence = [
     green,
     red,
     yellow,
     blue
 ];
+const flash = panel => {
+    return new Promise((resolve, reject) => {
+        panel.className += ' active';
+        setTimeout(() => {
+            panel.className = panel.className.replace(
+                ' active',
+                ''
+            );
+            resolve();
+        }, 1000);
+    });
+};
 
+const main = async () => {
+    for (const panel of sequence) {
+        await flash(panel);
+    }
+};
+
+main();
