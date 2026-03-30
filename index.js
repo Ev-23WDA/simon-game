8731888ace7b72656e9abe475da6416106f00aca
Ev-23WDA <evelacioabreu@gmail.com>
2026-03-30 09:09:34 +0100

    JS const

---
+++ index.js
@@ -0,0 +1,12 @@
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+
+const sequence = [
+    green,
+    red,
+    yellow,
+    blue
+];
+
